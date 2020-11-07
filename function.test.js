8b3e8a61188007a26f6d657ba92ae8cbc2ec6b6b
Michał Anioł <michal.janusz.aniol@gmail.com>
2020-11-07 15:52:46 +0100

    toClear

--- function.test.js
+++ function.test.js
@@ -8,38 +8,41 @@ console.log(toPhp('mixed_styleText'))
 console.log(toConstant('mixed_styleText'))
  */
 
-const testArr = [
-  {
-    origin: 'it_is_awesome',
-    camel: 'itIsAwesome',
-    php: 'it_is_awesome',
-    constant: 'IT_IS_AWESOME',
-    pascal: 'ItIsAwesome',
-    kebab: 'it-is-awesome'
-  },
-  {
-    origin: 'it',
-    camel: 'it',
-    php: 'it',
-    constant: 'IT',
-    pascal: 'It',
-    kebab: 'it'
-  },
-  {
-    origin: 'ItIsAwesome',
-    camel: 'itIsAwesome',
-    php: 'it_is_awesome',
-    constant: 'IT_IS_AWESOME',
-    pascal: 'ItIsAwesome',
-    kebab: 'it-is-awesome'
-  }
+const testArr = [{
+        origin: 'it_is_awesome',
+        camel: 'itIsAwesome',
+        php: 'it_is_awesome',
+        constant: 'IT_IS_AWESOME',
+        pascal: 'ItIsAwesome',
+        kebab: 'it-is-awesome',
+        clear: 'itisawesome'
+    },
+    {
+        origin: 'it',
+        camel: 'it',
+        php: 'it',
+        constant: 'IT',
+        pascal: 'It',
+        kebab: 'it',
+        clear: 'it'
+    },
+    {
+        origin: 'ItIsAwesome',
+        camel: 'itIsAwesome',
+        php: 'it_is_awesome',
+        constant: 'IT_IS_AWESOME',
+        pascal: 'ItIsAwesome',
+        kebab: 'it-is-awesome',
+        clear: 'itisawesome'
+    }
 ]
 test('Test Transform', () => {
-  testArr.forEach((item) => {
-    expect(util.toCamelCase(item.origin)).toBe(item.camel)
-    expect(util.toConstant(item.origin)).toBe(item.constant)
-    expect(util.toPascal(item.origin)).toBe(item.pascal)
-    expect(util.toPhp(item.origin)).toBe(item.php)
-    expect(util.toKebab(item.origin)).toBe(item.kebab)
-  })
+    testArr.forEach((item) => {
+        expect(util.toCamelCase(item.origin)).toBe(item.camel)
+        expect(util.toConstant(item.origin)).toBe(item.constant)
+        expect(util.toPascal(item.origin)).toBe(item.pascal)
+        expect(util.toPhp(item.origin)).toBe(item.php)
+        expect(util.toKebab(item.origin)).toBe(item.kebab)
+        expect(util.toClear(item.origin)).toBe(item.clear)
+    })
 })
